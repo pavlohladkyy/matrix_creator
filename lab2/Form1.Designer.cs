4ac856f00af5c33aebc91d07294efba653cb11ca
Павло Гладкий <hladkyy.p@nltu.lviv.ua>
2024-03-20 17:19:20 +0200

    Add project files.

---
+++ lab2/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxRow = new System.Windows.Forms.TextBox();
+            this.textBoxCol = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.CountSum = new System.Windows.Forms.Button();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "параметри матриці:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(14, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "К-сть рядків:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Location = new System.Drawing.Point(12, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "К-сть стовпців:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Location = new System.Drawing.Point(355, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Мінімальне значення елемента:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Location = new System.Drawing.Point(355, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Максимальне значення елемента:";
+            // 
+            // textBoxRow
+            // 
+            this.textBoxRow.Location = new System.Drawing.Point(166, 74);
+            this.textBoxRow.Name = "textBoxRow";
+            this.textBoxRow.Size = new System.Drawing.Size(108, 22);
+            this.textBoxRow.TabIndex = 5;
+            // 
+            // textBoxCol
+            // 
+            this.textBoxCol.Location = new System.Drawing.Point(166, 111);
+            this.textBoxCol.Name = "textBoxCol";
+            this.textBoxCol.Size = new System.Drawing.Size(108, 22);
+            this.textBoxCol.TabIndex = 6;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(637, 71);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(108, 22);
+            this.textBoxMin.TabIndex = 7;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(637, 111);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(108, 22);
+            this.textBoxMax.TabIndex = 8;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonCreate.Location = new System.Drawing.Point(15, 163);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(333, 26);
+            this.buttonCreate.TabIndex = 9;
+            this.buttonCreate.Text = "Створити матрицю";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonSort.Location = new System.Drawing.Point(409, 163);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(336, 26);
+            this.buttonSort.TabIndex = 10;
+            this.buttonSort.Tag = "";
+            this.buttonSort.Text = "сортувати матрицю за характристиками";
+            this.buttonSort.UseCompatibleTextRendering = true;
+            this.buttonSort.UseMnemonic = false;
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(15, 239);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(730, 268);
+            this.dataGrid.TabIndex = 11;
+            // 
+            // CountSum
+            // 
+            this.CountSum.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.CountSum.Location = new System.Drawing.Point(17, 207);
+            this.CountSum.Name = "CountSum";
+            this.CountSum.Size = new System.Drawing.Size(331, 26);
+            this.CountSum.TabIndex = 12;
+            this.CountSum.Text = "обчислити";
+            this.CountSum.UseVisualStyleBackColor = true;
+            this.CountSum.Click += new System.EventHandler(this.CountSum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(766, 529);
+            this.Controls.Add(this.CountSum);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxCol);
+            this.Controls.Add(this.textBoxRow);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "робота з масивами";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxRow;
+        private System.Windows.Forms.TextBox textBoxCol;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button CountSum;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+    }
+}
+
---
+++ lab2/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace lab2
+{
+#region Form
+    public partial class Form1 : Form
+    {
+        CharacteristicMatrix matrix;
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeTooltips();
+
+
+
+
+
+        }
+        private void InitializeTooltips()
+        {
+            
+            toolTip2.SetToolTip(buttonSort, "сортування за сумою модулів відємних непарних елеменів переставляючи стовпці матриці у їх відповідності з ростом характеристик");
+            toolTip3.SetToolTip(CountSum, "Обчислення суми елементів у тих стовпцях, які містять хоча б один від’ємний\r\nелемент.");
+        }
+
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBoxRow.Text, out var row))
+            {
+                MessageBox.Show("invalid row");
+                return;
+            }
+            if (!int.TryParse(textBoxCol.Text, out var col))
+            {
+                MessageBox.Show("invalid col");
+                return;
+            }
+            
+            matrix = new CharacteristicMatrix(row, col);
+
+            if (!int.TryParse(textBoxMin.Text, out var min))
+            {
+                MessageBox.Show("invalid min");
+                return;
+            }
+            if (!int.TryParse(textBoxMax.Text, out var max))
+            {
+                MessageBox.Show("invalid max");
+                return;
+            }
+
+            // Ensure that min is less than or equal to max
+            if (min > max)
+            {
+                // Swap min and max values if min is greater than max
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+
+            matrix.FillElementRandom(min, max);
+            Print(matrix);
+        }
+
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            if (matrix == null)
+            {
+                MessageBox.Show("Please create a matrix first.");
+                return;
+            }
+
+            // Calculate characteristic for each column
+            List<int> characteristics = new List<int>();
+            for (int j = 0; j < matrix.ColCount; j++)
+            {
+                int sum = 0;
+                for (int i = 0; i < matrix.RowCount; i++)
+                {
+                    int value = matrix[i, j];
+                    if (value < 0 && value % 2 != 0)
+                    {
+                        sum += Math.Abs(value);
+                    }
+                }
+                characteristics.Add(sum);
+            }
+
+            // Sort columns based on characteristics
+            var sortedColumns = characteristics.Select((x, i) => new KeyValuePair<int, int>(i, x))
+                                                .OrderBy(x => x.Value)
+                                                .Select(x => x.Key)
+                                                .ToArray();
+
+            // Rearrange matrix columns
+            CharacteristicMatrix sortedMatrix = new CharacteristicMatrix(matrix.RowCount, matrix.ColCount);
+            for (int j = 0; j < sortedColumns.Length; j++)
+            {
+                for (int i = 0; i < matrix.RowCount; i++)
+                {
+                    sortedMatrix[i, j] = matrix[i, sortedColumns[j]];
+                }
+            }
+
+            // Print sorted matrix
+            Print(sortedMatrix);
+
+            
+        }
+
+        private void Print(CharacteristicMatrix matrix)
+        {
+            DataTable dt = new DataTable();
+            int columns = matrix.ColCount;
+            int rows = matrix.RowCount;
+
+            // Add columns to the DataTable
+            for (int i = 0; i < columns; i++)
+            {
+                dt.Columns.Add(i.ToString(), typeof(double));
+            }
+
+            // Add rows to the DataTable
+            for (int row = 0; row < rows; row++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int col = 0; col < columns; col++)
+                {
+                    dr[col] = matrix[row, col];
+                }
+                dt.Rows.Add(dr);
+            }
+
+            dataGrid.DataSource = dt; // Set the DataSource property directly
+            dataGrid.ColumnHeadersVisible = false; // Hide column headers
+            dataGrid.RowHeadersVisible = false; // Hide row headers
+            dataGrid.AllowUserToAddRows = false; // Prevent users from adding rows
+        }
+
+        private void CountSum_Click(object sender, EventArgs e)
+        {
+            if (matrix == null)
+            {
+                MessageBox.Show("Please create a matrix first.");
+                return;
+            }
+
+            int sumOfNegativeColumns = 0;
+            for (int j = 0; j < matrix.ColCount; j++)
+            {
+                bool hasNegative = false;
+                for (int i = 0; i < matrix.RowCount; i++)
+                {
+                    if (matrix[i, j] < 0)
+                    {
+                        hasNegative = true;
+                        break;
+                    }
+                }
+                if (hasNegative)
+                {
+                    for (int i = 0; i < matrix.RowCount; i++)
+                    {
+                        sumOfNegativeColumns += matrix[i, j];
+                    }
+                }
+            }
+            MessageBox.Show($"Сума елементів у стовпцях, які містять хоча б один негативний елемент: {sumOfNegativeColumns}");
+        }
+
+        
+    }
+    #endregion
+
+    #region class
+    class CharacteristicMatrix
+    {
+        #region поля
+        int[,] matrix;
+        #endregion
+
+        #region властивості та індексатори
+        public int RowCount { get; }
+        public int ColCount { get; }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < RowCount && i >= 0 && j < ColCount && j >= 0)
+                    return matrix[i, j];
+                else throw new IndexOutOfRangeException("Індекс виходить за межі масиву!");
+            }
+            set
+            {
+                if (i < RowCount && i >= 0 && j < ColCount && j >= 0)
+                    matrix[i, j] = value;
+                else throw new IndexOutOfRangeException("Індекс виходить за межі масиву!");
+            }
+        }
+        #endregion
+
+        #region Конструктор
+        public CharacteristicMatrix(int row, int col)
+        {
+            matrix = new int[row, col];
+            RowCount = row;
+            ColCount = col;
+        }
+        #endregion
+
+        #region Методи
+        public void FillElementRandom(int min, int max)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < RowCount; i++)
+            {
+                for (int j = 0; j < ColCount; j++)
+                {
+                    matrix[i, j] = rand.Next(min, max + 1);
+                }
+            }
+        }
+        #endregion
+    }
+    #endregion
+}
